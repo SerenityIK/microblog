afaf44821b815a16696a569bf7946ee4513182a1
Irakliy Krasnov <serenity597th@gmail.com>
2020-10-09 17:12:18 +0300

    Add jwt tokens

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
-from app.email import send_password_reset_email
+# from app.email import send_password_reset_email
 
 
 @app.before_request
