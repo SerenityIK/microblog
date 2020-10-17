afb1d8ff7088218799183bbd6f3f512d6212cbab
Irakliy Krasnov <serenity597th@gmail.com>
2020-10-17 21:42:37 +0300

    Add translation marks

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_materialize import Material
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 import logging
 import os
 
@@ -18,6 +18,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page')
 mail = Mail(app)
 # init materialize css framework
 material = Material(app)
@@ -55,7 +56,7 @@ if not app.debug:
         app.logger.info('Microblog startup')
 
 
-@babel.localselector
+@babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
