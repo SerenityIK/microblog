0f882af5bdfa5630a8077350c91005c5d73fccee
Irakliy Krasnov <serenity597th@gmail.com>
2020-10-20 15:54:45 +0300

    Add post translations working with ms translator api

---
+++ app/translate.py
@@ -0,0 +1,31 @@
+import requests
+from flask_babel import _
+from app import app
+
+
+def translate(text, source_language, dest_language):
+    if 'MS_TRANSLATOR_KEY' not in app.config or \
+            not app.config['MS_TRANSLATOR_KEY']:
+        return _('Error: the translation service is not configured.')
+    region = 'global'
+    # set endpoint url
+    endpoint = 'https://api.cognitive.microsofttranslator.com/'
+    path = 'translate?api-version=3.0'
+    params = '&from={}&to={}'.format(source_language, dest_language)
+    constructed_url = endpoint + path + params
+    # set headers
+    headers = {
+        'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY'],
+        'Ocp-Apim-Subscription-Region': region,
+        'Content-type': 'application/json'
+        }
+    # set body
+    body = [{
+        'text': text
+    }]
+    # make request to ms translator api
+    response = requests.post(constructed_url, headers=headers, json=body)
+    if response.status_code != 200:
+        return _('Error: the translation service failed.')
+    translation = response.json()
+    return translation[0]['translations'][0]['text']
