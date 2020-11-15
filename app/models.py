b8dce85f3fd9118720631844668d2bf4c2465078
Irakliy Krasnov <serenity597th@gmail.com>
2020-11-15 22:03:00 +0200

    Add tasks queue with redis

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 import jwt
 import json
+import redis
+import rq
 from app import current_app, db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -8,7 +10,6 @@ from app import login
 from hashlib import md5
 from time import time
 from app.search import add_to_index, remove_from_index, query_index
-from time import time
 
 
 class SearchableMixin(object):
@@ -83,6 +84,7 @@ class User(UserMixin, db.Model):
     last_message_read_time = db.Column(db.DateTime)
     notifications = db.relationship('Notification', backref='user',
                                     lazy='dynamic')
+    tasks = db.relationship('Task', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -144,6 +146,21 @@ class User(UserMixin, db.Model):
         db.session.add(n)
         return n
 
+    def launch_task(self, name, description, *args, **kwargs):
+        rq_job = current_app.task_queue.enqueue('app.tasks. ' + name, self.id,
+                                                *args, **kwargs)
+        task = Task(id=rq_job.get_id(), name=name, description=description,
+                    user=self)
+        db.session.add(task)
+        return task
+
+    def get_tasks_in_progress(self):
+        return Task.query.filter_by(user=self, complete=False).all()
+
+    def get_task_in_progress(self, name):
+        return Task.query.filter_by(name=name, user=self,
+                                    complete=False).first()
+
 
 class Post(SearchableMixin, db.Model):
     __searchable__ = ['body']
@@ -177,6 +194,25 @@ class Notification(db.Model):
 
     def get_data(self):
         return json.loads(str(self.payload_json))
+
+
+class Task(db.Model):
+    id = db.Column(db.String(36), primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    description = db.Column(db.String(128))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    complete = db.Column(db.Boolean, default=False)
+
+    def get_rq_job(self):
+        try:
+            rq_job = rq.job.Job.fetch(self.id, connection=current_app.redis)
+        except (redis.exceptions.Redis.Error, rq.exceptions.NoSuchJobError):
+            return None
+        return rq_job
+
+    def get_progress(self):
+        job = self.get_rq_job()
+        return job.meta.get('progress', 0) if job is not None else 100
 
 
 @login.user_loader
